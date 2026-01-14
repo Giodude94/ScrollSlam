ea4a6ba104efd817af153a43c44069bd785fd8e0
Giodude94 <Giovanni_vega94@yahoo.com>
2026-01-14 17:12:51 -0600

    Updated Slam Logic
    Fixed behavior where player did not slam correctly after reaching apex.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -46,6 +46,19 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    [Header("Slam Charges")]
+    [SerializeField] int maxSlams = 7;
+
+    [Header("Ground Bounce")]
+    [SerializeField] float groundBounceMultiplier = 0.75f;
+    [SerializeField] float minGroundBounceForce = 8f;
+
+    //int currentSlams;
+
+    int currentSlams = 7;
+    bool isSlamming;
+    bool isGrounded;
+
     //[SerializeField] private float maxUpwardSpeed = 80f;
     //[SerializeField] private float maxDownwardSpeed = -12f;
     //[SerializeField] float maxHeight = 25f;
@@ -65,9 +78,21 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if(state != PlayerState.Idle) { return; }
+        if(state == PlayerState.Idle) 
+        {
+            HandleLaunchCharge();
+            return; 
+        }
 
-        HandleLaunchCharge();
+        HandleSlamInput();
+    }
+
+    void HandleSlamInput()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            TrySlam();
+        }
     }
     private void FixedUpdate()
     {
@@ -105,9 +130,10 @@ public class PlayerController : MonoBehaviour
     {
         if (state != PlayerState.Idle) { return; }
 
+        currentSlams = maxSlams;
+
         state = PlayerState.Running;
 
-        //rb.simulated = true;
         rb.constraints = RigidbodyConstraints2D.None;
         rb.gravityScale = 1f;
         rb.velocity = Vector2.zero;
@@ -136,16 +162,18 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void Slam()
+    void TrySlam()
     {
-        if(state != PlayerState.Running) { return; }
+        if (currentSlams <= 0) { return; }
 
-        //Cancelling upward momentum befor slamming
-        if (rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0f);
-        }
+        if(isGrounded) { return; }
 
+        Debug.Log("Slam activating");
+
+        currentSlams--;
+        isSlamming = true;
+
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
         rb.AddForce(Vector2.down * slamForce, ForceMode2D.Impulse);
 
     }
@@ -159,10 +187,46 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y * ceilingBounceDampening);
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+
+            if (isSlamming)
+            {
+                HandleGroundBounce(collision);
+            }
+        }
+        HandleGroundBounce(collision);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void HandleGroundBounce(Collision2D collision)
+    {
+        isSlamming = false;
+
+        float impactSpeed = Mathf.Abs(rb.velocity.y);
+        
+        float bounceForce = Mathf.Max(impactSpeed * groundBounceMultiplier, minGroundBounceForce);
+
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+    } 
     public void BounceOffEnemy(Vector2 contactPoint)
     {
-        Debug.Log("Bounce Off Enemy is called");
+        
         if (state != PlayerState.Running) { return; }
+
+        currentSlams = Mathf.Min(currentSlams + 1, maxSlams);
 
         //Cancelling downward velocity before bounce
         if (rb.velocity.y < 0f) 
