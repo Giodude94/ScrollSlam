93fd6e8db5bae20d8666f52e031470ad3b23d865
Giodude94 <Giovanni_vega94@yahoo.com>
2026-01-26 17:18:28 -0600

    Fixed settings to spawn enemies once again.

--- Assets/Scripts/ChunkTrigger.cs
+++ Assets/Scripts/ChunkTrigger.cs
@@ -18,6 +18,7 @@ public class ChunkTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         if (triggered) { return; }
 
         if (!other.CompareTag("Player")) { return; }
